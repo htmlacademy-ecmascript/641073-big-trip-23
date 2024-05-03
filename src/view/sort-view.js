04d4fae709e902117c3c67c3af38739858593972
Valentina A <valentina.aktanko@local-internet.ru>
2024-05-03 09:33:06 +0300

    fix sort view

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,47 +1,12 @@
 import { createElement } from '../render.js';
 
-const SORT_TYPES = [
-  {
-    value: 'day',
-    label: 'Day',
-    sortable: true,
-    selected: false,
-  },
-  {
-    value: 'event',
-    label: 'Event',
-    sortable: false,
-    selected: false,
-  },
-  {
-    value: 'time',
-    label: 'Time',
-    sortable: true,
-    selected: false,
-  },
-  {
-    value: 'price',
-    label: 'Price',
-    sortable: true,
-    selected: true,
-  },
-  {
-    value: 'offer',
-    label: 'Offers',
-    sortable: false,
-    selected: false,
-  },
-];
+const SORT_TYPES = ['day', 'event', 'time', 'price', 'offer'];
 
 function createSortItemTemplate(type) {
-  const { value, label, sortable, selected } = type;
-  const disabled = sortable ? '' : 'disabled';
-  const checked = selected ? 'checked' : '';
-
   return `
-    <div class="trip-sort__item  trip-sort__item--${value}">
-      <input id="sort-${value}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${value}" ${disabled} ${checked}>
-      <label class="trip-sort__btn" for="sort-${value}">${label}</label>
+    <div class="trip-sort__item  trip-sort__item--${type}">
+      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}">
+      <label class="trip-sort__btn" for="sort-${type}">${type}</label>
     </div>
   `;
 }
